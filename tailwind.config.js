61da16f8327b5a5c6b0ac0b54b0b0c5a030b3e17
Daniel Zebini <danielzebini@hotmail.com>
2022-05-03 15:05:32 -0300

    Full page

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          100: '#FBF3ED',
+          200: '#FFD8CD',
+          300: '#FE6236',
+          400: '#DB834C',
+          500: '#B86A3E',
+          600: '#983B20',
+        },
+        gray: {
+          100: '#EBEBEB',
+          200: '#C4C4C4',
+          300: '#5F6368',
+          400: '#333333',
+          500: '#16141C',
+        },
+        yellow: {
+          100: '#F2C94C',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
